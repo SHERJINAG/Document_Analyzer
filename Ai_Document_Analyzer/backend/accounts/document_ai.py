a71bae8438aaafdfa598fde1f92abfc4798f691c
SHERJIN A G <146447202+SHERJINAG@users.noreply.github.com>
2026-04-03 12:18:15 +0530

    Update document_ai.py

--- Ai_Document_Analyzer/backend/accounts/document_ai.py
+++ Ai_Document_Analyzer/backend/accounts/document_ai.py
@@ -254,18 +254,7 @@ def analyze_sentiment(text):
 import subprocess
 import logging
 
-def check_system():
-    try:
-        tesseract_version = subprocess.getoutput("tesseract --version")
-        logging.info(f"TESSERACT VERSION: {tesseract_version}")
-    except Exception as e:
-        logging.error(f"Tesseract error: {e}")
 
-    try:
-        path = subprocess.getoutput("which tesseract")
-        logging.info(f"TESSERACT PATH: {path}")
-    except Exception as e:
-        logging.error(f"Path error: {e}")
 # =========================
 # MAIN PROCESS
 # =========================
@@ -279,7 +268,7 @@ def process_document(file_path):
 
     if not text.strip():
         return {"status": "error", "message": "No text extracted"}
-    check_system()
+   
     summary = summarize_text(text)
     entities = extract_entities_cleaned(text)
     sentiment = analyze_sentiment(text)
