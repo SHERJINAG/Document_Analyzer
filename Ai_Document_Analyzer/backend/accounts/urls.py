3a179ab78db8a0955d1f7de0781ae683ff175208
SHERJIN A G <146447202+SHERJINAG@users.noreply.github.com>
2026-04-03 21:57:15 +0530

    Update urls.py

--- Ai_Document_Analyzer/backend/accounts/urls.py
+++ Ai_Document_Analyzer/backend/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register_view, login_view, upload_document,my_documents,system_check
+from .views import register_view, login_view, upload_document,my_documents
 
 urlpatterns = [
     path("register/", register_view),
@@ -17,7 +17,7 @@ urlpatterns = [
         "my-documents/",
         my_documents,
         name="my_documents"
-    ),path("system-check/", system_check),
+    ),
     
    
 ]
