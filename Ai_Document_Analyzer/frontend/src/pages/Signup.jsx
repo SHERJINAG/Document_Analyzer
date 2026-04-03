3ab9a1f2925e3be30b98e394b4fa095de3905228
SHERJIN A G <146447202+SHERJINAG@users.noreply.github.com>
2026-04-03 10:41:51 +0530

    Update Signup.jsx

--- Ai_Document_Analyzer/frontend/src/pages/Signup.jsx
+++ Ai_Document_Analyzer/frontend/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function Signup() {
   const [fullName, setFullName] = useState("");
